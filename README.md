57f8499e8effd09228c5d9e6fb95af6abc0b6edd
lk <kkdyx@vip.qq.com>
2016-10-03 12:14:33 +0800

    '游戏编码完成'

--- README.md
+++ README.md
@@ -35,3 +35,23 @@ mainWindow = new BrowserWindow({
 基本上模拟2048App的外观
 
 ![https://raw.githubusercontent.com/lkathous/Electron-2048/master/img/样式.png?raw=true](https://raw.githubusercontent.com/lkathous/Electron-2048/master/img/样式.png?raw=true)
+
+## API设计
+### getMatrix()
+获取游戏方块数字的二维数组，比如（-1代表没有方块）：
+```JavaScript
+[
+	[16, 4, 2, -1],
+	[32, 4, 2, -1],
+	[4, 2, -1, -1],
+	[2, -1, -1, -1]
+]
+```
+### initializeGame()
+初始化游戏数据，初始化游戏界面
+
+### createCell()
+空白处创建一个新方块在，9成几率是2，1成几率是4
+
+### slide(dirction)
+滑动操作，dirction可以是"↑", "↓", "←", "→"字符串，分别对应四个方向上的滑动。
--- js/app.js
+++ js/app.js
@@ -1,13 +1,124 @@
-const {ipcRenderer} = require('electron')
+console.log("app.js..");
 
-console.log("test app");
+let ipcRenderer
+try {
+  ipcRenderer = require('electron').ipcRenderer
+} catch (e) {
+  ipcRenderer = {}
+}
 
-let closeWindow = function() {
+let doAnimate = (cell, classN) => {
+  classN = " " + classN
+  cell.className = cell.className + classN
+  setTimeout(() => {
+    cell.className = cell.className.replace(classN, "")
+  }, 500)
+}
+
+let game = new Game()
+game.initializeGame()
+
+// 用户操作接口
+let closeWindow = () => {
   console.log('closeWindow')
   ipcRenderer.send('close-window')
 }
 
-let minimizeWindow = function() {
+let minimizeWindow = () => {
   console.log('minimizeWindow')
   ipcRenderer.send('minimize-window')
+}
+
+let start = () => {
+  console.log('start')
+  game.initializeGame()
+}
+
+let test = () => {
+  game.reset()
+}
+
+document.onkeydown = (e) => {
+  if (event.ctrlKey && event.altKey) {
+    shortKey(e.keyCode)
+    return
+  }
+
+  var code = window.event ? e.keyCode : e.which
+  let dirction = "";
+  switch(code) {
+    case 38:
+    case 87:
+      dirction = "↑"
+      break;
+    case 40:
+    case 83:
+      dirction = "↓"
+      break;
+    case 37:
+    case 65:
+      dirction = "←"
+      break;
+    case 39:
+    case 68:
+      dirction = "→"
+      break;
+  }
+  if (dirction === "") return
+  game.slide(dirction)
+}
+
+// 模拟操作部分
+let cancelAuto = false
+let keydown = code => {
+  CustomEvent = document.createEvent('HTMLEvents')
+  CustomEvent.initEvent('keydown', !1, !1)
+  CustomEvent.keyCode = code
+  document.dispatchEvent(CustomEvent)
+}
+
+let autoKeyDown = str => {
+  for (var i = 0; i < str.length; i++) {
+    let char = str.charAt(i)
+    switch(char) {
+      case "w":
+      case "0":
+        keydown(38)
+        break;
+      case "s":
+      case "1":
+        keydown(40)
+        break;
+      case "a":
+      case "2":
+        keydown(37)
+        break;
+      case "d":
+      case "3":
+        keydown(39)
+        break;
+    }
+  }
+}
+
+let shortKey = code => {
+  console.log("==== "+code);
+  cancelAuto = false
+  switch(code) {
+    case 75:
+      autoRandom()
+      break;
+    case 80:
+      cancelAuto = true
+      break;
+  }
+}
+let autoRandom = () => {
+  if (cancelAuto) return
+  setTimeout(() => {
+    console.log("hehe");
+    let code = parseInt(Math.random() * 4)
+    autoKeyDown("sdsdsdsdsdsdsdsdsdsdsdsdsdsdas")
+    return autoRandom()
+  }, 100)
 }
